d31307e416d4a47aaf4397d2c0981fa958dc4518
Anthony Ponson <54106169+anthonyponson@users.noreply.github.com>
2023-04-12 01:15:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Login from './Login'
 import Form from './Form'
 import { stateContext } from './Context'
+import { initialState, stateReducer } from './Reducer'
 
 const App = () => {
+  const [state, dispatch] = useReducer(stateReducer, initialState)
+
   return (
     <>
-      <stateContext.Provider>
+      <stateContext.Provider value={{ state, dispatch }}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />}></Route>
